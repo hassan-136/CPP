739e7b6366dbae907c6962f77be6aa00dc0083d1
hassan-136 <hjamal.bscs24seecs@seecs.edu.pk>
2025-09-15 22:18:17 +0500

    K

--- Pointers_Swapping.cpp
+++ Pointers_Swapping.cpp
@@ -13,37 +13,76 @@
 //     return 0;
 // }
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int rows = 3, cols = 4;
+
+//     // create array of int pointers
+//     int** arr = new int*[rows];
+
+//     // allocate each row
+//     for(int i=0; i<rows; i++) {
+//         arr[i] = new int[cols];
+//     }
+
+//     // fill array
+//     for(int i=0; i<rows; i++) {
+//         for(int j=0; j<cols; j++) {
+//             cin>>arr[i][j];
+//         }
+//     }
+
+//     // print array
+//     for(int i=0; i<rows; i++) {
+//         for(int j=0; j<cols; j++) {
+//             cout << arr[i][j] << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     // delete memory
+//     for(int i=0; i<rows; i++) {
+//         delete[] arr[i];  // delete each row
+//     }
+//     delete[] arr;         // delete array of pointers
+// }
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    int rows = 3, cols = 4;
+    int *rows = new int;
+    cin>>*rows;
+    int *cols=new int;
+    cin>>*cols;
 
     // create array of int pointers
-    int** arr = new int*[rows];
+    int** arr = new int*[*rows];
 
     // allocate each row
-    for(int i=0; i<rows; i++) {
-        arr[i] = new int[cols];
+    for(int i=0; i<*rows; i++) {
+        arr[i] = new int[*cols];
     }
 
     // fill array
-    for(int i=0; i<rows; i++) {
-        for(int j=0; j<cols; j++) {
+    for(int i=0; i<*rows; i++) {
+        for(int j=0; j<*cols; j++) {
             cin>>arr[i][j];
         }
     }
 
     // print array
-    for(int i=0; i<rows; i++) {
-        for(int j=0; j<cols; j++) {
+    for(int i=0; i<*rows; i++) {
+        for(int j=0; j<*cols; j++) {
             cout << arr[i][j] << " ";
         }
         cout << endl;
     }
 
     // delete memory
-    for(int i=0; i<rows; i++) {
+    for(int i=0; i<*rows; i++) {
         delete[] arr[i];  // delete each row
     }
     delete[] arr;         // delete array of pointers
